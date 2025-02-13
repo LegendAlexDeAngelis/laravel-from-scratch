63a15bc793ea0a679896bae23e922355e2eea79b
Alex De Angelis <alex.deangelis@L1.com>
2025-02-13 16:24:51 +0000

    input validation and errors

--- workopia/resources/views/jobs/create.blade.php
+++ workopia/resources/views/jobs/create.blade.php
@@ -1,10 +1,21 @@
 <x-layout>
-  <x-slot name="title">Create new job</x-slot>
-  <h1>Create new job</h1>
-  <form action="/jobs" method="POST">
-    @csrf
-    <input type="text" name="title" placeholder="Job Title">
-    <input type="text" name="description" placeholder="Job Description">
-    <button type="submit">Submit</button>
-  </form>
+    <x-slot name="title">Create new job</x-slot>
+    <h1>Create new job</h1>
+    <form action="/jobs" method="POST">
+        @csrf
+        <div class="my-5">
+            <input type="text" name="title" placeholder="Job Title" value="{{ old('title') }}">
+            @error('title')
+                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-5">
+            <input type="text" name="description" placeholder="Job Description" value="{{ old('description') }}">
+            @error('description')
+                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <button type="submit">Submit</button>
+    </form>
 </x-layout>
